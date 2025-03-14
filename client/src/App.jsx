ff4ee265677a058bb7ca8e2d30b3d741271c20e3
Muralidhara Bhat KS <ksmuralidhara0@gmail.com>
2025-03-14 22:11:38 +0530

    init client side

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import { Box, Button } from "@chakra-ui/react"
+import { Route, Routes } from "react-router-dom"
+
+function App() {
+  return (
+    <Box minH={"100vh"}>
+      {/* <Navbar /> */}
+
+      <Routes>
+        <Route />
+      </Routes>
+    </Box>
+  )
+}
+
+export default App
